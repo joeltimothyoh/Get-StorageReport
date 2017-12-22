5b2d04a7134b10c4961e1a78920d25477c59a379
Joel Timothy Oh <joeltimothyoh@live.com>
2017-12-22 22:59:02 +0800

    Add module. Add parameters. Tweak threshold percent. Tweak formatting of report. Update documentation and readme. Update .gitignore

--- Get-Storage-Report.ps1
+++ Get-Storage-Report.ps1
@@ -1,14 +1,38 @@
 ﻿<#
 .SYNOPSIS
-Generates and emails a report regarding the storage of local drives on the system.
+Generates a report regarding the storage status of local logical drives on the system.
 
 .DESCRIPTION
-The report will warn when any of the monitored local drive's free space falls below the set quota.
+The report will include a warning when one or more local logical drives' free space falls below the set threshold.
+
+.PARAMETER Drive
+Logical drive(s) to get the storage status of.
+
+.PARAMETER Threshold
+The threshold for free space in percent below which a warning would be issued.
 
 .EXAMPLE
-.\Get-Storage-Report.ps1
+Powershell "C:\scripts\Get-Storage-Report\Get-Storage-Report.ps1"
+Runs the Get-Storage-Report.ps1 script in an instance of PowerShell.
 
+.EXAMPLE
+Get-Storage-Report -Drive C:, D: -Threshold 10
+Runs the Get-Storage-Report module to get the storage status of C: and D:, with a specified free space threshold of 10%.
+
+.LINK
+https://github.com/joeltimothyoh/Get-Storage-Report
 #>
+
+####################   Get-Storage-Report Settings   ####################
+
+# Logical drive(s) to get the storage status of (If unspecified, monitors all drives)
+$monitored_drives = @(
+  # 'C:'
+  # 'D:'
+)
+
+# Threshold for free space in percent below which a warning is issued.
+$freespace_threshold_percent = '10.0'
 
 ##########################   Email Settings   ###########################
 
@@ -33,40 +57,52 @@ $email_to = 'recipient@address.com'
 # Email title prefix
 $email_title_prefix = '[MachineName]'
 
-####################   Get-Storage-Report Settings   ####################
-
-# Local logical drives to monitor (if unspecified, monitors all local logical drives)
-$monitored_drives = @(
-    # 'C:'
-    # 'D:'
-)
-
-# Free space threshold before warning is issued (e.g. 0.1 for 10% free space)
-$free_space_threshold = '0.2'
-
 #########################################################################
 
 function Get-Storage-Report {
 
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$False)]
+        [alias("d")]
+        # Throw a more meaningful error message than if ValidatePattern was used
+        [ValidateScript( {
+            if ($_ -match "^[A-Za-z]:$") {
+                $true
+            } else {
+                Throw "The arguments `"$_`" does not match the `"X:`" pattern. Supply an argument that matches `"X:`" and try the command again."
+            }
+        } )]
+        [string[]]$Drive
+        ,
+        [Parameter(Mandatory=$False)]
+        [alias("t")]
+        [ValidateRange(0,100)]
+        [float]$Threshold
+    )
+
     # Get info of local logical drives
-    if ($monitored_drives.count -gt 0) {
-        $Logical_Drives_Info = Get-WmiObject -Class Win32_Logicaldisk | Where-Object { ($monitored_drives -contains $_.DeviceID) -And (($_.DriveType -eq 2) -Or ($_.DriveType -eq 3)) }
+    if ($Drive.count -gt 0) {
+        $Logical_Drives_Info = Get-WmiObject -Class Win32_Logicaldisk | Where-Object { ($Drive -contains $_.DeviceID) -And (($_.DriveType -eq 2) -Or ($_.DriveType -eq 3)) }
 
     } else {
         $Logical_Drives_Info = Get-WmiObject -Class Win32_Logicaldisk | Where-Object { (($_.DriveType -eq 2) -Or ($_.DriveType -eq 3)) }
     }
 
     # Count the number of logical drives low on free space
-    $full_drives = $Logical_Drives_Info | Where-Object { ($_.FreeSpace/$_.Size) -le $free_space_threshold} | Measure-Object
+    $full_drives = $Logical_Drives_Info | Where-Object { ($_.FreeSpace/$_.Size)*100 -le $Threshold} | Measure-Object
 
     # Generate a table containing each monitored logical drive with their respective capacities
-    $drive_capacity_table = $Logical_Drives_Info | 
-        Format-Table DeviceID,                                                                                              # DeviceID for drive letter
-                     VolumeName,                                                                                            # VolumeName for logical volume name
-                     @{ Name = "Size (GB)"; Expression = { [math]::Round($_.Size/1GB,2) } },                                # Size for logical volume size
-                     @{ Name = "FreeSpace (GB)"; Expression = { [math]::Round($_.FreeSpace/1GB,2) } },                      # FreeSpace for logical volume free space
-                     @{ Name = "FreeSpace (%)"; Expression = { "{0:P1}" -f ($_.FreeSpace/$_.Size) }; Alignment="right" }    # FreeSpace (%) for logical volume free space percent
-        
+    $drive_capacity_table = $Logical_Drives_Info | Format-Table `
+        DeviceID,                                                                                              # DeviceID for drive letter
+        VolumeName,                                                                                            # VolumeName for logical volume name
+        @{ Name = "Size (GB)"; Expression = { [math]::Round($_.Size/1GB,2) } },                                # Size for logical volume size
+        @{ Name = "FreeSpace (GB)"; Expression = { [math]::Round($_.FreeSpace/1GB,2) } },                      # FreeSpace for logical volume free space
+        @{ Name = "FreeSpace (%)"; Expression = { "{0:P1}" -f ($_.FreeSpace/$_.Size) }; Alignment="right" }    # FreeSpace (%) for logical volume free space percent
+
+    # Trim newlines in the formatted table
+    $drive_capacity_table = ($drive_capacity_table | Out-String).Trim()
+
     # Module name to appear in title
     $module_name = "[Get-Storage-Report]"
 
@@ -81,13 +117,14 @@ function Get-Storage-Report {
     # Format body of report
     $drive_capacity_report = @(
         "Drive Capacity Check as of: $(Get-Date)"
-        "Free Space Threshold: $([math]::Round([float]$free_space_threshold * 100))%"
+        "Free Space Threshold: $Threshold%"
         $drive_capacity_table
     )
 
     # Print report the stdout
     Write-Output $title
     Write-Output $drive_capacity_report
+    Write-Output "-"
 
     # Format title of email report
     $email_title_prefix = $email_title_prefix.Trim()
@@ -104,14 +141,36 @@ function Get-Storage-Report {
         "</p></pre>"
     )
 
-    # Secure credentials
+    # Secure credential
     $encrypted_password = $smtp_password | ConvertTo-SecureString -AsPlainText -Force
-    $credentials = New-Object System.Management.Automation.PSCredential( $smtp_email, $encrypted_password )
-    
+    $credential = New-Object System.Management.Automation.PSCredential( $smtp_email, $encrypted_password )
+
+    # Define Send-Mailmessage parameters
+    $emailprm = @{
+        SmtpServer = $smtp_server
+        Port = $smtp_port
+        UseSsl = $true
+        Credential = $credential
+        From = $email_from
+        To = $email_to
+        Subject = $email_title
+        Body = ($email_body | Out-String)
+        BodyAsHtml = $true
+    }
+
     # Email the report
-    Send-Mailmessage -to $email_to -subject $email_title -Body ( $email_body | Out-String ) -from $email_from -SmtpServer $smtp_server -Port $smtp_port -Credential $credentials -UseSsl -BodyAsHtml
+    try {
+        Send-Mailmessage @emailprm -ErrorAction Stop
+    } catch {
+        Write-Output "Failed to send email. Reason: $($_.Exception.Message)"
+    }
 
 }
 
-# Call function
-Get-Storage-Report
+# Trim the array only if it is not empty
+if ($monitored_drives.count -gt 0) {
+     $monitored_drives = $monitored_drives.Trim()
+}
+
+# Call main function
+Get-Storage-Report -Drive $monitored_drives -Threshold $freespace_threshold_percent
---
+++ Module/Get-Storage-Report/Get-Storage-Report.psm1
@@ -0,0 +1,95 @@
+﻿function Get-Storage-Report {
+    <#
+    .SYNOPSIS
+    Generates a report regarding the storage status of local logical drives on the system.
+
+    .DESCRIPTION
+    The report will include a warning when one or more local logical drives' free space falls below the set threshold.
+
+    .PARAMETER Drive
+    Logical drive(s) to get the storage status of.
+
+    .PARAMETER Threshold
+    The threshold for free space in percent below which a warning would be issued.
+
+    .EXAMPLE
+    Powershell "C:\scripts\Get-Storage-Report\Get-Storage-Report.ps1"
+    Runs the Get-Storage-Report.ps1 script in an instance of PowerShell.
+
+    .EXAMPLE
+    Get-Storage-Report -Drive C:, D: -Threshold 10
+    Runs the Get-Storage-Report module to get the storage status of C: and D:, with a specified free space threshold of 10%.
+
+    .LINK
+    https://github.com/joeltimothyoh/Get-Storage-Report
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$False)]
+        [alias("d")]
+        # Throw a more meaningful error message than if ValidatePattern was used
+        [ValidateScript( {
+            if ($_ -match "^[A-Za-z]:$") {
+                $true
+            } else {
+                Throw "The arguments `"$_`" does not match the `"X:`" pattern. Supply an argument that matches `"X:`" and try the command again."
+            }
+        } )]
+        [string[]]$Drive
+        ,
+        [Parameter(Mandatory=$False)]
+        [alias("t")]
+        [ValidateRange(0,100)]
+        [float]$Threshold
+    )
+
+    # Get info of local logical drives
+    if ($Drive.count -gt 0) {
+        $Logical_Drives_Info = Get-WmiObject -Class Win32_Logicaldisk | Where-Object { ($Drive -contains $_.DeviceID) -And (($_.DriveType -eq 2) -Or ($_.DriveType -eq 3)) }
+
+    } else {
+        $Logical_Drives_Info = Get-WmiObject -Class Win32_Logicaldisk | Where-Object { (($_.DriveType -eq 2) -Or ($_.DriveType -eq 3)) }
+    }
+
+    # Count the number of logical drives low on free space
+    $full_drives = $Logical_Drives_Info | Where-Object { ($_.FreeSpace/$_.Size)*100 -le $Threshold} | Measure-Object
+
+    # Generate a table containing each monitored logical drive with their respective capacities
+    $drive_capacity_table = $Logical_Drives_Info | Format-Table `
+        DeviceID,                                                                                              # DeviceID for drive letter
+        VolumeName,                                                                                            # VolumeName for logical volume name
+        @{ Name = "Size (GB)"; Expression = { [math]::Round($_.Size/1GB,2) } },                                # Size for logical volume size
+        @{ Name = "FreeSpace (GB)"; Expression = { [math]::Round($_.FreeSpace/1GB,2) } },                      # FreeSpace for logical volume free space
+        @{ Name = "FreeSpace (%)"; Expression = { "{0:P1}" -f ($_.FreeSpace/$_.Size) }; Alignment="right" }    # FreeSpace (%) for logical volume free space percent
+
+    # Trim the new lines in the formatted table
+    $drive_capacity_table = ($drive_capacity_table | Out-String).Trim()
+
+    # Module name to appear in title
+    $module_name = "[Get-Storage-Report]"
+
+    # Format title of report
+    $title = "$module_name "
+    if ($full_drives.Count -gt 0) {
+        $title += "$($full_drives.Count) Drive(s) are getting full."
+    } else {
+        $title += "No problems found."
+    }
+
+    # Format body of report
+    $drive_capacity_report = @(
+        "Drive Capacity Check as of: $(Get-Date)"
+        "Free Space Threshold: $Threshold%"
+        $drive_capacity_table
+    )
+
+    # Print report the stdout
+    Write-Output $title
+    Write-Output $drive_capacity_report
+    Write-Output "-"
+
+}
+
+# Export the members of the module
+Export-ModuleMember -Function Get-Storage-Report
